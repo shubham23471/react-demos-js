b0f488addb563a64219b55241f8b53ad4d411980
shubham <shubhamshakya61@gmail.com>
2024-11-14 12:19:11 +0530

    useState updating parent component from child

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import Weather from "./components/Weather";
+import ComponentOne from "./components/ComponentOne";
+import ComponentTwo from "./components/ComponentTwo";
+import { useState } from "react";
 
 const App = () => {
-  const tempValue = 15;
-  return <Weather temperatue={tempValue}></Weather>;
+  // TODO: Update this count from child component
+  const [count, setCount] = useState(0);
+  const updateClickHandler = () => setCount(count + 100);
+  return (
+    <div>
+      <ComponentOne
+        count={count}
+        setCount={setCount}
+        updateClickHandler={updateClickHandler}
+      />
+      <ComponentTwo count={count} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ComponentTwo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentTwo = ({ count }) => {
+  return <div>Value of count from ComponentTwo: {count}</div>;
+};
+
+export default ComponentTwo;
