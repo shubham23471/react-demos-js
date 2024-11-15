e6ac80134589d1a8730dadb7122872c4b5a1f70c
shubham <shubhamshakya61@gmail.com>
2024-11-15 17:43:24 +0530

    useEffect basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import TodoList from "./components/TodoList";
-import { useState } from "react";
-import Counter from "./components/Counter";
-import TodoListForm from "./components/TodoListForm";
-import Profile from "./components/Profile";
-import ShoppingList from "./components/ShoppingList";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState(0);
+  const [something, setSomething] = useState(0);
+
+  // useEffect function with depencendy array.
+  // this function will be called every single
+  // time when you click "Click Me"
+  // useEffect(() => {
+  //   console.log("call useEffect");
+  //   document.title = `Increment ${value}`;
+  // });
+
+  // with empty dependency array
+  // now you will only see `call useEffect` on
+  // on the console on intial render.
+  // useEffect(() => {
+  //   console.log("call useEffect");
+  //   document.title = `Increment ${value}`;
+  // }, []);
+
+  // useEffect based on some value
+  // any-time the state of `value` changes
+  // then useEffect will call the callback function
+  useEffect(() => {
+    console.log("call useEffect");
+    document.title = `Increment ${value}`;
+  }, [value]);
+
   return (
     <div>
-      {/* <p>------- Simple Counter-------</p>
-      <Counter></Counter>
-      <p>--------------</p>
-      <p>------- Static Item update-------</p>
-      <TodoList> </TodoList>
-      <p>--------------</p> */}
-      {/* <TodoListForm></TodoListForm> */}
-      {/* <Profile></Profile> */}
-      <ShoppingList></ShoppingList>
+      <h2>{value}</h2>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+      <button onClick={() => setSomething(value + 1)}>
+        Increment based value of `something`
+      </button>
     </div>
   );
 };
