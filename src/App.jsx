fda4247f11c9959ff727e54f425b8fbaa197f3c9
shubham <shubhamshakya61@gmail.com>
2024-11-16 17:23:32 +0530

    fetch api without custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import Counter from "./components/Counter";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  // only want to render this content once: []
+  // fetch the data and store it data var using setData
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div>
-      {" "}
-      <Counter></Counter>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
     </div>
   );
 };
